831a4af6657738f41af52c63329f4822f9d0f4bd
runzac <91915526+alerunza@users.noreply.github.com>
2024-03-17 10:47:19 +0100

    fix(font): syntax

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,23 +5,9 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-      'sans': [
-        'ui-sans-serif', 
-        'system-ui', 
-        '-apple-system', 
-        'BlinkMacSystemFont', 
-        'Segoe UI', 
-        'Roboto', 
-        'Helvetica Neue', 
-        'Arial', 
-        'Noto Sans', 
-        'sans-serif', 
-        'Apple Color Emoji', 
-        'Segoe UI Emoji', 
-        'Segoe UI Symbol', 
-        'Noto Color Emoji'
-      ],
-    },
+        'body': ['ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
+        'heading': ['ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
+      },
       colors: {
         transparent: "transparent",
         current: "currentColor",
